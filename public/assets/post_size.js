1d53460f135c21099bb281e6dfc1e076fab14b63
Mikhail <test@test.com>
2023-07-23 14:51:07 +0300

    Создал структуру проекта соглачно шаблона

--- public/assets/post_size.js
+++ public/assets/post_size.js
@@ -1,41 +1,41 @@
+function isLink(str) {
+  const dictionary = [
+    '.ru',
+    '.su',
+    '.рф',
+    '.com',
+    '.net',
+    '.name ',
+    '.com.ru',
+    '.net.ru',
+    '.org',
+    '.ru',
+    '.com',
+    '.cyou',
+    '.net',
+    '.biz',
+    '.info',
+    '.name',
+    '.site',
+  ];
+
+  for (const i of dictionary) {
+    if (str.endsWith(i)) {
+      str = '';
+    }
+  }
+
+  return str;
+}
+
 export default function postSize(post) {
-  let arr = post.split(" ");
-  let arrNoLinks = [];
+  const arr = post.split(' ');
+  const arrNoLinks = [];
 
   arr.forEach(function (item) {
     arrNoLinks.push(isLink(item));
   });
-  post = arrNoLinks.join(" ");
+  post = arrNoLinks.join(' ');
 
   return post.length;
 }
-
-
-function isLink(str) {
-  let dictionary = [
-    ".ru",
-    ".su",
-    ".рф",
-    ".com",
-    ".net",
-    ".name ",
-    ".com.ru",
-    ".net.ru",
-    ".org",
-    ".ru",
-    ".com",
-    ".cyou",
-    ".net",
-    ".biz",
-    ".info",
-    ".name",
-    ".site",
-  ];
-  for (let i of dictionary) {
-    if (str.endsWith(i)) {
-      str = "";
-    }
-  }
-  return str;
-}
-
