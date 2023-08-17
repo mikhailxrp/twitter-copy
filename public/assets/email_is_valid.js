b4a5d8c5015385866ed2b5ca3defaf3f5d9d4c51
Mikhail <test@test.com>
2023-08-17 16:09:28 +0300

    Добавил новое условие для валидации email

--- public/assets/email_is_valid.js
+++ public/assets/email_is_valid.js
@@ -1,7 +1,8 @@
 export default function isValidEmail(str) {
+
   str = str.trim().toLowerCase();
 
-  if (str.includes('@') && str.includes('.') && !str.includes('@.')) {
+  if (str.includes('@') && str.includes('.') && !str.includes('@.') && str[0] != '@') {
     return true;
   } else {
     return false;
