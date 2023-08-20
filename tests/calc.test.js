8efcb6e542571b01ee88fec0870519c1da46d1db
Mikhail <test@test.com>
2023-08-20 13:00:08 +0300

    Написал функцию определения времени публикации поста, а так же тестов к этой функции

--- tests/calc.test.js
+++ tests/calc.test.js
@@ -2,6 +2,7 @@ import { assert } from 'chai';
 import postSize from '../public/assets/post_size.js';
 import replaceLink from '../public/assets/replace_link.js';
 import isValidEmail from '../public/assets/email_is_valid.js';
+import postTime from '../public/assets/post_time.js'
 
 
 describe('Функция проверки расчета размера поста', function () {
@@ -89,4 +90,32 @@ describe('Функция проверки email', function () {
   it('Email без имени', function(){
     assert.equal(isValidEmail('@test.ru'), false);
   });
+});
+
+describe('Функция времени публикации', function () {
+  it('10 минут', function () {
+    assert.equal(postTime(10), '10 минут назад');
+  });
+  it('3 минуты', function () {
+    assert.equal(postTime(3), '3 минуты назад');
+  });
+  it('48 минут', function () {
+    assert.equal(postTime(48), '48 минут назад');
+  });
+  it('1 час', function () {
+    assert.equal(postTime(61), '1 час назад');
+  });
+  it('10 часов', function () {
+    assert.equal(postTime(610), '10 часов назад');
+  });
+  it('1 день', function () {
+    assert.equal(postTime(1445), '1 день назад');
+  });
+  it('3 дня', function () {
+    assert.equal(postTime(4325), '3 дня назад');
+  });
+  it('1 год', function () {
+    assert.equal(postTime(525605), 'более года назад');
+  });
+  
 });
