86bd52f74110b1aa4331eb039c52f34161979e20
Mikhail <test@test.com>
2023-08-30 13:19:10 +0300

    добавил пробелы в файле calc.test.js

--- tests/calc.test.js
+++ tests/calc.test.js
@@ -2,8 +2,7 @@ import { assert } from 'chai';
 import postSize from '../public/assets/post_size.js';
 import replaceLink from '../public/assets/replace_link.js';
 import isValidEmail from '../public/assets/email_is_valid.js';
-import postTime from '../public/assets/post_time.js'
-
+import postTime from '../public/assets/post_time.js';
 
 describe('Функция проверки расчета размера поста', function () {
   it('без ссылок', function () {
@@ -72,22 +71,22 @@ describe('Функция замены ссылок на html', function () {
 });
 
 describe('Функция проверки email', function () {
-  it('Корректный email', function(){
-    assert.equal(isValidEmail('test@test.com'), true)
+  it('Корректный email', function () {
+    assert.equal(isValidEmail('test@test.com'), true);
   });
-  it('Email без домена', function(){
-    assert.equal(isValidEmail('test@test'), false)
+  it('Email без домена', function () {
+    assert.equal(isValidEmail('test@test'), false);
   });
-  it('Email без знака @', function(){
-    assert.equal(isValidEmail('test#test.ru'), false)
+  it('Email без знака @', function () {
+    assert.equal(isValidEmail('test#test.ru'), false);
   });
-  it('Email заглавными', function(){
-    assert.equal(isValidEmail('TEST@TEST.COM'), true)
+  it('Email заглавными', function () {
+    assert.equal(isValidEmail('TEST@TEST.COM'), true);
   });
-  it('Email без названия домена', function(){
+  it('Email без названия домена', function () {
     assert.equal(isValidEmail('test@.ru'), false);
   });
-  it('Email без имени', function(){
+  it('Email без имени', function () {
     assert.equal(isValidEmail('@test.ru'), false);
   });
 });
@@ -117,5 +116,4 @@ describe('Функция времени публикации', function () {
   it('1 год', function () {
     assert.equal(postTime(525605), 'более года назад');
   });
-  
 });
