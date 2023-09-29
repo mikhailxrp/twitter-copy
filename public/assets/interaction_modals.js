2a52b5933cb70fa187bd5d2f0144851ee564544c
Mikhail <test@test.com>
2023-09-29 19:22:42 +0300

    Разделил функцию взаимодействия с модальными окнами на отдельные функции

--- public/assets/interaction_modals.js
+++ public/assets/interaction_modals.js
@@ -1,18 +1,15 @@
 // Работа с модальными окнами
-export default function interactionModal() {
-  // Работа с модальными окнами
-  const signUpBtns = document.querySelectorAll('.btn-sign-up');
-  const loginBtns = document.querySelectorAll('.btn-login');
-  const overlay = document.querySelector('#overlay');
-  const closeBtns = document.querySelectorAll('[data-close="close"]');
-  const registerWindow = document.querySelector('[data-modal="register"]');
-  const loginWindow = document.querySelector('[data-modal="login"]');
+const signUpBtns = document.querySelectorAll('.btn-sign-up');
+const loginBtns = document.querySelectorAll('.btn-login');
+const overlay = document.querySelector('#overlay');
+const closeBtns = document.querySelectorAll('[data-close="close"]');
+const registerWindow = document.querySelector('[data-modal="register"]');
+const loginWindow = document.querySelector('[data-modal="login"]');
 
+export default function interactionModal() {
   signUpBtns.forEach((item) => {
     item.addEventListener('click', function () {
-      overlay.classList.add('visible');
-      registerWindow.classList.add('active');
-      document.body.classList.add('no-scroll');
+      openWindow(registerWindow, overlay);
 
       if (registerWindow.classList.contains('no-active')) {
         registerWindow.classList.remove('no-active');
@@ -22,9 +19,7 @@ export default function interactionModal() {
 
   loginBtns.forEach((item) => {
     item.addEventListener('click', function () {
-      overlay.classList.add('visible');
-      loginWindow.classList.add('active');
-      document.body.classList.add('no-scroll');
+      openWindow(loginWindow, overlay);
 
       if (loginWindow.classList.contains('no-active')) {
         loginWindow.classList.remove('no-active');
@@ -33,12 +28,7 @@ export default function interactionModal() {
   });
 
   closeBtns.forEach(function (close) {
-    close.addEventListener('click', function () {
-      overlay.classList.remove('visible');
-      registerWindow.classList.remove('active');
-      loginWindow.classList.remove('active');
-      document.body.classList.remove('no-scroll');
-    });
+    close.addEventListener('click', closeWindow);
   });
 
   // если ширина экрана меньше 575px
@@ -46,41 +36,54 @@ export default function interactionModal() {
     // Свайп по окну регистрации или логина
     document.addEventListener('touchstart', touchStart, false);
     document.addEventListener('touchmove', touchMove, false);
+  }
+}
 
-    let y1 = null;
+function disableActivity() {
+  setTimeout(function () {
+    loginWindow.classList.remove('active');
+    overlay.classList.remove('visible');
+    document.body.classList.remove('no-scroll');
+  }, 200);
+}
 
-    function touchStart(event) {
-      const firstTouch = event.touches[0];
-      y1 = firstTouch.clientY;
-    }
+let y1 = null;
 
-    function touchMove(event) {
-      if (!y1) {
-        return false;
-      }
+function touchStart(event) {
+  const firstTouch = event.touches[0];
+  y1 = firstTouch.clientY;
+}
 
-      let y2 = event.touches[0].clientY;
+function touchMove(event) {
+  if (!y1) {
+    return false;
+  }
 
-      let yDiff = y2 - y1;
+  let y2 = event.touches[0].clientY;
 
-      if (Math.abs(yDiff)) {
-        if (yDiff > 0 && loginWindow.classList.contains('active')) {
-          loginWindow.classList.add('no-active');
-          setTimeout(function () {
-            loginWindow.classList.remove('active');
-            overlay.classList.remove('visible');
-            document.body.classList.remove('no-scroll');
-          }, 200);
-        } else if (yDiff > 0 && registerWindow.classList.contains('active')) {
-          registerWindow.classList.add('no-active');
-          setTimeout(function () {
-            loginWindow.classList.remove('active');
-            overlay.classList.remove('visible');
-            document.body.classList.remove('no-scroll');
-          }, 200);
-        }
-      }
-      y1 = null;
+  let yDiff = y2 - y1;
+
+  if (Math.abs(yDiff)) {
+    if (yDiff > 0 && loginWindow.classList.contains('active')) {
+      loginWindow.classList.add('no-active');
+      disableActivity();
+    } else if (yDiff > 0 && registerWindow.classList.contains('active')) {
+      registerWindow.classList.add('no-active');
+      disableActivity();
     }
   }
+  y1 = null;
+}
+
+function closeWindow() {
+  overlay.classList.remove('visible');
+  registerWindow.classList.remove('active');
+  loginWindow.classList.remove('active');
+  document.body.classList.remove('no-scroll');
+}
+
+function openWindow(window, overlay) {
+  overlay.classList.add('visible');
+  window.classList.add('active');
+  document.body.classList.add('no-scroll');
 }
