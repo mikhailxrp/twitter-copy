ef92e3110b09eed162c1379c4f6de4c8473ea89e
Mikhail <test@test.com>
2023-10-04 18:23:23 +0300

    Написал модуль валидации формы регистрации

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,7 +1,9 @@
 import postSize from './post_size.js';
 import interactionModal from './interaction_modals.js';
+import registerFormValidation from './registerValidationForm.js';
 
 interactionModal()
+registerFormValidation()
 
 alert(postSize('Это новый пост'));
 
