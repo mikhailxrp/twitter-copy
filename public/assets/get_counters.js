408598783b837d501072044e836e4e304e3a4963
Mikhail <test@test.com>
2023-10-15 23:23:48 +0300

    аписал функцию получения и рендера счетчиков на главной странице

---
+++ public/assets/get_counters.js
@@ -0,0 +1,25 @@
+export default function () {
+  getCounters();
+}
+
+async function getCounters() {
+  try {
+    const response = fetch('/public/data.json');
+    const data = (await response).json();
+    const res = await data;
+    
+    for(let itemCount in res.static){
+      render(res.static[itemCount].count, res.static[itemCount].text);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function render(count, text) {
+  const markup = `<div class="count-inner">
+                    <div class="count" data-count-users>${count}</div>
+                    <div class="count-title">${text}</div>
+                  </div>`;
+   document.getElementById('counterWrapper').insertAdjacentHTML('beforeend', markup);
+}
