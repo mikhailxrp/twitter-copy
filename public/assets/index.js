41d4cfc31b50ae1d0fd53477071b4d40994a12b8
Mikhail <test@test.com>
2023-11-05 17:43:49 +0300

    написал модуль получения и рендеринга данных боковой панели на главной странице

--- public/assets/index.js
+++ public/assets/index.js
@@ -3,10 +3,12 @@ import interactionModal from './interaction_modals.js';
 import registerFormValidation from './registerValidationForm.js';
 import counterRender from './get_counters.js';
 import getMessages from './messages/get_messages.js'
+import getContentPage from './get_page_content.js'
 
 interactionModal();
 registerFormValidation();
 counterRender();
 getMessages()
+getContentPage();
 
 // alert(postSize('Это новый пост'));
