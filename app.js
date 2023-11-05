e4cb075aafca384b760cdf46f9c81e782bf35a00
Mikhail <test@test.com>
2023-11-05 22:52:43 +0300

    установил express создал app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import fs from 'fs';
+
+const app = express()
+const port = 3000
+
+const html = fs.writeFileSync('public/main.html', 'utf8');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.type('html').send(html));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
