a3ed7e61bed0857f70f5d4a523ee8640f72a5452
Mikhail <test@test.com>
2023-12-27 19:14:30 +0300

    Исправил запросы к БД и изменил Таблицу с аватарами, добавил аватары в таблицу к пользователям

--- db/controller.js
+++ db/controller.js
@@ -6,7 +6,7 @@ export async function getUsers(req, res) {
   const users = await pool.query('SELECT * FROM public.users');
   res.status(200).json(users.rows);
 }
-
+// переделать в одну таблицу с пользователями
 export async function getUserAvatar(req, res) {
   const avatars = await pool.query('SELECT * FROM public.user_avatar');
   res.status(200).json(avatars.rows);
@@ -60,22 +60,22 @@ export async function createUser(req, res) {
   let name;
   let email;
   let state;
-  const userData = await pool.query('SELECT user_name, user_email FROM public.users');
 
-  for (let user of userData.rows) {
-    if (user.user_name === req.body.user_name) {
-      text = 'Пользователь с таким именем зарегистрирован';
-      status = 400;
-      state = false;
-      name = text;
-    } else if (user.user_email === req.body.user_email) {
-      text = 'Пользователь с таким email зарегистрирован';
-      status = 400;
-      state = false;
-      email = text;
-    } else {
-      state = true;
-    }
+  let userEmail = await pool.query('SELECT user_email FROM public.users WHERE user_email=$1', [req.body.user_email]);
+  let userName = await pool.query('SELECT user_name FROM public.users WHERE user_name=$1', [req.body.user_name]);
+
+  if (userName.rows.length !== 0) {
+    text = 'Пользователь с таким именем зарегистрирован';
+    status = 400;
+    state = false;
+    name = text;
+  } else if (userEmail.rows.length !== 0) {
+    text = 'Пользователь с таким email зарегистрирован';
+    status = 400;
+    state = false;
+    email = text;
+  } else {
+    state = true;
   }
 
   if (state) {
@@ -94,26 +94,24 @@ export async function isUser(req, res) {
   let state;
   const { user_email, user_password } = req.body;
 
-  const users = await pool.query('SELECT * FROM public.users');
+  let user = await pool.query('SELECT * FROM public.users WHERE user_email=$1', [user_email]);
 
-  for (let userItem of users.rows) {
-
-    if (userItem.user_email !== user_email) {
-      status = 401;
-      message = 'Пользователь с таким email не найден';
-      state = false;
-    } else if(!bcrypt.compareSync(user_password, userItem.user_password)){
-      status = 400
-      message = 'Пароли не совпадают'
-      state = false
-    }else{
-      state = true;
-    }
+  if (user.rows.length === 0) {
+    status = 401;
+    message = 'Пользователь с таким email не найден';
+    state = false;
+  } else if (!bcrypt.compareSync(user_password, user.rows[0].user_password)) {
+    status = 400;
+    message = 'Пароли не совпадают';
+    state = false;
+  } else {
+    state = true;
   }
 
   if (state) {
     status = 200;
     message = 'OK';
+    console.log(user.rows[0]);
   }
 
   res.status(status).json({ status: status, message: message });
