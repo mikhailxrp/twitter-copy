e78d2f09c1a0aa43b0c13f1a6adb747890e3fba9
Mikhail <dmihail528@gmail.com>
2024-01-29 01:36:48 +0300

    Изменил путь к файлу для главной страницы

--- routers/server.js
+++ routers/server.js
@@ -26,7 +26,7 @@ router.use(tokenVerification);
 router.get("/", authCheck);
 
 // Главная страница для сервера
-const index = fs.readFileSync("public/index.html", "utf8");
+const index = fs.readFileSync("public/main.html", "utf8");
 router.get("/app", (req, res) => res.type("html").send(index));
 
 // получаю пользователей
