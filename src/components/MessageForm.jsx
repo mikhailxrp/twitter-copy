a17ee0b790792f339b965d79e59828ad1a25c0ae
Mikhail <dmihail528@gmail.com>
2024-02-12 17:59:40 +0300

    Реализовал функционал подсчета символов в сообщении

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,19 +1,46 @@
+import { useState } from 'react';
+import postSize from '/public/assets/post_size.js'
 import camera from '../img/topics/camera.svg'
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import './style/formControl.css'
 
-const percentage = 200;
+
 
 const MessageForm = () => {
+    const [activeForm, setActiveForm] = useState(false)
+    const [message, setMessage] = useState('')
+    const [percentage, setPercentage] = useState(0)
+    
+
+    function handleActive(){
+        setActiveForm(true)
+    }
+
+    function handleAddMessage(event){ 
+        setMessage(event.target.value);
+        setPercentage(postSize(event.target.value))
+        
+    }
+
+
+    
     
     return <>
     <form className="message-form">
-        <div className="what-new">
+        <div className="what-new" onClick={handleActive} >
             <label htmlFor='message' className='question-message'>Что нового Александр..?</label>
         </div>
 
-        <div className="message-form-control">
-            <input type="text" placeholder="Сообщение" id='message'/>
+        <div className={activeForm ? "message-form-control active" :"message-form-control"} >
+            <textarea 
+                type="text" 
+                placeholder="Сообщение" 
+                id='message' 
+                value={message} 
+                onChange={handleAddMessage} 
+            />
+
             <div className="message-form-control-wrapper">
                 <button type='button' className="message-form-img">
                     <img src={camera} alt="" />
