3b8423c05ba913213c345995c027b9fac084b4c3
Mikhail <dmihail528@gmail.com>
2024-03-25 23:32:38 +0300

    финальная реализация взаимодествия с модальным окном

--- src/components/FormMobileMessage.jsx
+++ src/components/FormMobileMessage.jsx
@@ -2,13 +2,14 @@ import "./style/footer.css"
 import { useState } from "react";
 import { CircularProgressbar } from 'react-circular-progressbar';
 import { Widget } from '@uploadcare/react-widget'
-import { useDispatch} from "react-redux";
-// import { fetchAddPosts, fetchPosts } from "../store/postsSlice";
+import { useDispatch } from "react-redux";
+import { fetchAddPosts, fetchPosts } from "../store/postsSlice";
 
 
-const FormMobileMessage = ({activeForm, closeForm}) => {
+const FormMobileMessage = ({ activeForm, closeForm }) => {
     const [percentage, setPercentage] = useState(0)
     const [message, setMessage] = useState('')
+    const [image, setImage] = useState('')
 
     const dispatch = useDispatch()
 
@@ -20,19 +21,19 @@ const FormMobileMessage = ({activeForm, closeForm}) => {
     // Отправка сообщения
     const submitMessage = async (event) => {
         event.preventDefault()
+        console.log('click');
 
-        // const response = await dispatch(fetchAddPosts({message: message, image: image}))
+        const response = await dispatch(fetchAddPosts({ message: message, image: image }))
         await dispatch(fetchPosts())
 
-        if(response.payload.status === 200){
+        if (response.payload.status === 200) {
             setMessage('')
             setPercentage(0)
             closeForm()
-        }else {
+        } else {
             alert(response.payload.error)
         }
-        
-        
+
     }
 
     // касание формы
@@ -40,7 +41,7 @@ const FormMobileMessage = ({activeForm, closeForm}) => {
     const touchStart = (event) => {
         const firstTouch = event.touches[0];
         y1 = firstTouch.clientY;
-        
+
     }
 
     // Движение вниз
@@ -52,40 +53,40 @@ const FormMobileMessage = ({activeForm, closeForm}) => {
 
         let yDiff = y2 - y1;
 
-        if(Math.abs(yDiff)){
-            if(yDiff > 0){
+        if (Math.abs(yDiff)) {
+            if (yDiff > 0) {
                 closeForm()
             }
         }
 
         y1 = null;
     }
-    
+
     return <>
-     <form 
-        className={activeForm ? "message-form-mobile active" : "message-form-mobile"}
-        onTouchStart={touchStart}
-        onTouchMove={momeScreen}
-        onSubmit={submitMessage}
-     >
-        <div className="form-mobile-divider"></div>
-        <div className="form-control-mobile">
-            <input 
-                type="text" 
-                placeholder="Сообщение" 
-                name="message"
-                value={message}
-                onChange={createNewMessage}
-            />
-        </div>
-        <div className='progress-wrapper-mobile'>
-            <Widget onChange={(info) => setImage(info)}/>
-            <div className='progress-box'> 
-                <CircularProgressbar value={percentage} maxValue={330} text={`${percentage}`} />
+        <form
+            className={activeForm ? "message-form-mobile active" : "message-form-mobile"}
+            onTouchStart={touchStart}
+            onTouchMove={momeScreen}
+            onSubmit={submitMessage}
+        >
+            <div className="form-mobile-divider"></div>
+            <div className="form-control-mobile">
+                <input
+                    type="text"
+                    placeholder="Сообщение"
+                    name="message"
+                    value={message}
+                    onChange={createNewMessage}
+                />
             </div>
-            <button className="btn btn-form-message-mobile ">Отправить</button>
-        </div>
-    </form>
+            <div className='progress-wrapper-mobile'>
+                <Widget onChange={(info) => setImage(info)} />
+                <div className='progress-box'>
+                    <CircularProgressbar value={percentage} maxValue={330} text={`${percentage}`} />
+                </div>
+                <button className="btn btn-form-message-mobile ">Отправить</button>
+            </div>
+        </form>
     </>
 }
 
--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,12 +1,11 @@
-import { useState} from 'react';
+import { useState } from 'react';
 import { Widget } from '@uploadcare/react-widget'
 import postSize from '/public/assets/post_size.js'
-import { useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 import { fetchAddPosts, fetchPosts } from "../store/postsSlice";
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import './style/formControl.css'
-
 
 
 const MessageForm = () => {
@@ -17,57 +16,56 @@ const MessageForm = () => {
 
     const dispatch = useDispatch()
 
-    function handleActive(){
+    function handleActive() {
         setActiveForm(true)
     }
 
-    function handleAddMessage(event){ 
+    function handleAddMessage(event) {
         setMessage(event.target.value);
         setPercentage(postSize(event.target.value))
     }
 
-   async function handleSendMessage(e){
+    async function handleSendMessage(e) {
         e.preventDefault()
-        const response = await dispatch(fetchAddPosts({message: message, image: image}))
+        const response = await dispatch(fetchAddPosts({ message: message, image: image }))
         await dispatch(fetchPosts())
 
-        if(response.payload.status === 200){
+        if (response.payload.status === 200) {
             setMessage('')
             setImage('')
             setPercentage(0)
-        }else {
+        } else {
             alert(response.payload.error)
         }
 
-        
     }
 
     return <>
-    <form className="message-form" onSubmit={handleSendMessage}>
-        <div className="what-new" onClick={handleActive} >
-            <label htmlFor='message' className='question-message'>Что нового Александр..?</label>
-        </div>
-
-        <div className={activeForm ? "message-form-control active" :"message-form-control"} >
-            <textarea 
-                type="text" 
-                placeholder="Сообщение" 
-                value={message} 
-                onChange={handleAddMessage} 
-
-            />
-
-            <div className="message-form-control-wrapper">
-                <Widget onChange={(info) => setImage(info.cdnUrl)} />
-               <div className='message-progress-wrapper'>
-                    <div className='progress-box'> 
-                        <CircularProgressbar value={percentage} maxValue={330} text={`${percentage}`} />
-                    </div>
-                    <button className="btn btn-form-message">Отправить</button>
-               </div>
+        <form className="message-form" onSubmit={handleSendMessage}>
+            <div className="what-new" onClick={handleActive} >
+                <label htmlFor='message' className='question-message'>Что нового Александр..?</label>
             </div>
-        </div>
-    </form>
+
+            <div className={activeForm ? "message-form-control active" : "message-form-control"} >
+                <textarea
+                    type="text"
+                    placeholder="Сообщение"
+                    value={message}
+                    onChange={handleAddMessage}
+
+                />
+
+                <div className="message-form-control-wrapper">
+                    <Widget onChange={(info) => setImage(info.cdnUrl)} />
+                    <div className='message-progress-wrapper'>
+                        <div className='progress-box'>
+                            <CircularProgressbar value={percentage} maxValue={330} text={`${percentage}`} />
+                        </div>
+                        <button className="btn btn-form-message">Отправить</button>
+                    </div>
+                </div>
+            </div>
+        </form>
     </>
 }
 
